e7c90c71070d94ee47e97f76e46305f51cb104ae
Kristian Santos <kristianmoacir@hotmail.com>
2022-06-14 12:00:38 -0300

    ci: adjust files

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "mars-rest-api",
+	Use:   "",
 	Short: "",
 	Long:  ``,
 }
